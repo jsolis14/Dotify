34ac2ac473688ecc5f924dce704a03eab77d38cd
Jesse Solis <jesse.solis0316@gmail.com>
2020-05-15 11:45:17 -0500

    env variable heroku

--- src/components/loginPage/LoginPage.js
+++ src/components/loginPage/LoginPage.js
@@ -4,7 +4,7 @@ function LoginPage() {
     function redirectToSpotify(e) {
         e.preventDefault();
         const scopes = encodeURIComponent("streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-library-read user-library-modify playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private")
-        const redirect_uri = 'http://localhost:3000/';
+        const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
         const client_id = 'ad099cca2b5444c59732b6696db3acb9';
         const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=token&state=123`
         window.location.href = url;
