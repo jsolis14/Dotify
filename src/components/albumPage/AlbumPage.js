4a7331aed076ec9ab714b5580f103ac1cdecc713
Jesse Solis <jesse.solis0316@gmail.com>
2020-07-13 16:19:43 -0500

    added styling to albumPage

--- src/components/albumPage/AlbumPage.js
+++ src/components/albumPage/AlbumPage.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect, useContext } from 'react'
 import { AppContext } from '../../context/appContext';
 import Table from 'react-bootstrap/Table';
 import SongItem from '../song/SongItem';
+import './albumPage.css';
 
 export default function AlbumPage(props) {
     const { authToken } = useContext(AppContext);
     const album_id = props.match.params.albumId;
-    const [album, setAlbum] = useState({ tracks: { items: [] } })
+    const [album, setAlbum] = useState({ tracks: { items: [] }, images: [], artists: [], album_type: '' })
+
+    function getArtist() {
+        const artists = album.artists;
+        const artistArr = artists.map(artist => artist.name);
+        return artistArr.join();
+    }
 
     const getAlbum = async () => {
         const url = `https://api.spotify.com/v1/albums/${album_id}`
@@ -30,6 +37,25 @@ export default function AlbumPage(props) {
 
     return (
         <>
+            <div className='album-page__top'>
+                <div className='album-page__top-left'>
+                    {album.images.length > 0 ? <img src={`${album.images[1].url}`} /> : <></>}
+                </div>
+                <div className='album-page__top-right'>
+                    <div className='album-page__album-type'>
+                        {album.album_type.toUpperCase()}
+                    </div>
+                    <div className='album-page__title'>
+                        {album.name}
+                    </div>
+                    <div className=''>
+                        {`By ${getArtist()}`}
+                    </div>
+                    <div className=''>
+                        {`${album.release_date} ᛫ ${album.total_tracks} songs `}
+                    </div>
+                </div>
+            </div>
             <div className='playlist-container'>
                 {/* <PlayListHeader playlistDetail={playlistDetail} /> */}
                 <Table style={{ tableLayout: 'fixed' }} className='playlist__table' striped bordered hover variant="dark">
